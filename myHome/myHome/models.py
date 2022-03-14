7e989549af47ef9c98a5d2bca8c4456c30fa22cd
Zhanylmyrza <93324684+Zhanylmyrza@users.noreply.github.com>
2022-03-14 14:49:34 +0600

    Add files via upload

---
+++ myHome/myHome/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+# class Passengers(models, Model):
+#     origin = models.CharField(max_length=74)
+#     destination = models.CharField(max_length=74)
+#     duration = models.IntegerField()
---
+++ product/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+  list_display=['title', 'updated','timestamp']
+  list_display_links=['title']
+  list_filter=['updated','timestamp']
+  search_fields=['title','description']
+  
+admin.site.register(Product,ProductAdmin)
+
